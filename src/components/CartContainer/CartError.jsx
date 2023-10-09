cc5d1411babf8948840bb3aa6c239f3b2240b1f3
MatiasCataldo <maticataldo21@gmail.com>
2023-10-09 09:50:08 -0300

    mejoras

--- src/components/CartContainer/CartError.jsx
+++ src/components/CartContainer/CartError.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom"
+
+
 const CartError = () => {
   return (
-        <div className="card-error">
-            <h3>El carrito está vacío</h3>
-            <h4>🛒</h4>
+        <div>
+            <article className="card-error">
+                <h3>El carrito está vacío</h3>
+                <h4>🛒</h4>
+            </article>
+            <button  className='btn btn-dark' id='seguirComprando'><Link to="/">Seguir Comprando</Link></button>
         </div>
     )
 }
